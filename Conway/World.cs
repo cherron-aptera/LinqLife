1deee4baf95ca6df15a951273c13f1c6a5270157
Clint Herron <cherron@apterainc.com>
2020-03-12 13:41:41 -0400

    Unit tests pass again.

--- Conway/World.cs
+++ Conway/World.cs
@@ -5,16 +5,28 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Conway
 {
-    public struct Coordinate
+    public struct Coordinate : IEquatable<Coordinate>
     {
         public int x;
         public int y;
+
+        public bool Equals([AllowNull] Coordinate other)
+        {
+            return x == other.x
+                && y == other.y;
+        }
     }
 
-    public struct Cell
+    public struct Cell : IEquatable<Cell>
     {
         public bool value;
         public Coordinate coord;
+
+        public bool Equals([AllowNull] Cell other)
+        {
+            return value == other.value
+                && coord.Equals(other.coord);
+        }
     }
 
     public class World : IEquatable<World>
@@ -25,28 +37,31 @@ namespace Conway
         {
             int x, y = 0;
 
-            foreach (string row in initData)
+            if (initData != null)
             {
-                x = 0;
-                foreach (char cellChar in row)
+                foreach (string row in initData)
                 {
-                    if(cellChar.Equals('#'))
+                    x = 0;
+                    foreach (char cellChar in row)
                     {
-                        var cell = new Cell()
+                        if (cellChar.Equals('#'))
                         {
-                            value = true,
-                            coord = new Coordinate()
+                            var cell = new Cell()
                             {
-                                x = x,
-                                y = y
-                            },
-                        };
-                        worldData.Add(cell.coord,
-                            cell);
+                                value = true,
+                                coord = new Coordinate()
+                                {
+                                    x = x,
+                                    y = y
+                                },
+                            };
+                            worldData.Add(cell.coord,
+                                cell);
+                        }
+                        x++;
                     }
-                    x++;
+                    y++;
                 }
-                y++;
             }
         }
 
@@ -57,7 +72,25 @@ namespace Conway
 
         public bool Equals([AllowNull] World other)
         {
-            // TODO: Compare worldData
+            // Look at all live cells
+            var hots = worldData.Values.Where(c => c.value);
+            var otherHots = other.worldData.Values.Where(c => c.value);
+
+            // Ensure that we have the same number of live cells
+            if (hots.Count() != otherHots.Count())
+                return false;
+
+            // Ensure that for each live cell, there is a corresponding live cell in the other list
+            foreach (Cell c in hots)
+            {
+                Cell oc;
+                if (!other.worldData.TryGetValue(c.coord, out oc))
+                    return false;
+
+                if (c.value != oc.value)
+                    return false;
+            }
+
             return true;
         }
     }
