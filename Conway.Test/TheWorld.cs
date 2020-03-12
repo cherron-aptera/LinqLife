863638e8cf4f3ba2edf34be680d002c2ee935c46
Clint Herron <cherron@apterainc.com>
2020-03-12 13:19:40 -0400

    Structure and first two unit tests pass.

---
+++ Conway.Test/TheWorld.cs
@@ -0,0 +1,57 @@
+using System;
+using Conway;
+using Xunit;
+
+namespace Conway.Test
+{
+    public class TheWorld
+    {
+        [Fact]
+        public void EmptyWorldDoesNotHaveSpontaneousGeneration()
+        {
+            World blankWorld = new World(new string[]
+                {
+                    "...",
+                    "...",
+                    "..."
+                }
+            );
+
+            var next = blankWorld.GetNext();
+
+            // Empty worlds should stay empty
+            Assert.Equal(blankWorld, next);
+
+            // However, we should not return the same instance
+            Assert.NotSame(blankWorld, next);
+        }
+
+        [Fact]
+        public void SingleCellDiesAlone()
+        {
+            World world1 = new World(new string[]
+                {
+                    "...",
+                    ".#.",
+                    "..."
+                }
+            );
+
+            World world2 = new World(new string[]
+                {
+                    "...",
+                    "...",
+                    "..."
+                }
+            );
+
+            var next = world1.GetNext();
+
+            // Single nodes should die off
+            Assert.Equal(world2, next);
+        }
+
+
+
+    }
+}
---
+++ Conway/World.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Conway
+{
+    public struct Coordinate
+    {
+        int x;
+        int y;
+    }
+
+    public struct Cell
+    {
+        bool value;
+    }
+
+    public class World : IEquatable<World>
+    {
+        private Dictionary<Coordinate, Cell> worldData = new Dictionary<Coordinate, Cell>();
+
+        public World(string[] initData = null)
+        {
+            // TODO: Populate worldData with initData
+        }
+
+        public World GetNext()
+        {
+            return new World();
+        }
+
+        public bool Equals([AllowNull] World other)
+        {
+            // TODO: Compare worldData
+            return true;
+        }
+    }
+}
